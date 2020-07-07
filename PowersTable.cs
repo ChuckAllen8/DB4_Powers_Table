512df53d2d31222f333d08e7c474d5c6d02a31ec
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-07-07 15:21:55 -0400

    Initial commit of the project

---
+++ PowersTable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB4_Powers_Table
+{
+    class PowersTable
+    {
+        private const int WIDTH = 15;
+        private string[] columns;
+        private string divider;
+
+        public PowersTable()
+        {
+            columns = new string[] { "Number", "Squared", "Cubed" };
+            divider = new string('=', WIDTH);
+        }
+
+        public void Start()
+        {
+            bool loop = true;
+            while(loop)
+            {
+                DisplayPowers(GetNumber());
+                loop = RunAgain();
+                Console.WriteLine("\n");
+            }
+        }
+
+        private int GetNumber()
+        {
+            int entry = 0;
+            string input;
+            
+            Console.Write("Please enter an integer: ");
+            input = Console.ReadLine();
+
+            while(!int.TryParse(input, out entry) || (entry <= 0 || entry > 1290))
+            {
+                Console.WriteLine("\nInvalid entry, it must be greater than 0 and less than 1290.");
+                Console.Write("Try again: ");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("");
+            return entry;
+        }
+
+        private bool RunAgain()
+        {
+            Console.Write("Continue? (y, anything else quits): ");
+            if(Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void DisplayPowers(int limit)
+        {
+            Console.WriteLine($"{columns[0],WIDTH}|{columns[1],WIDTH}|{columns[2],WIDTH}|");
+            Console.WriteLine($"{divider,WIDTH}|{divider,WIDTH}|{divider,WIDTH}|");
+            for (int number = 1; number <= limit; number++)
+            {
+                Console.WriteLine($"{number,WIDTH:#,#.##}|{number*number,WIDTH:#,#.##}|{number*number*number,WIDTH:#,#.##}|");
+            }
+            Console.WriteLine($"{divider,WIDTH}|{divider,WIDTH}|{divider,WIDTH}|");
+            Console.WriteLine($"");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DB4_Powers_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowersTable app = new PowersTable();
+            app.Start();
+        }
+    }
+}
